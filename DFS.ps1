9cdf332aa44cfee5bf632eabfd28d990396f5431
bgoapper <bgoapper@eni.fr>
2018-09-21 16:27:46 +0200

    Add files via upload

---
+++ DFS.ps1
@@ -0,0 +1,21 @@
+﻿Configuration DFS
+{
+    Node "localhost"
+    {
+        WindowsFeature "DFS Namespaces"
+        {
+            Ensure = "Present"
+            Name = "FS-DFS-Namespace"
+        }
+        WindowsFeature "DFS Replication"
+        {
+            Ensure = "Present"
+            Name = "FS-DFS-Replication"
+        }
+        WindowsFeature "DFS Management Tools"
+        {
+            Ensure = "Present"
+            Name = "RSAT-DFS-Mgmt-Con"
+        }
+    }
+}
